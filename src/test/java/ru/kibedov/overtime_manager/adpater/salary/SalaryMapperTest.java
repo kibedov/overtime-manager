4ca4b0035753ebb39fb358f0c0ca1dcde7ecb5fb
Ruslan Kibedov <kibedov.r96@gmail.com>
2024-06-15 13:05:13 +0300

    write test for SalaryMapper and refactor

--- src/test/java/ru/kibedov/overtime_manager/adpater/salary/SalaryMapperTest.java
+++ src/test/java/ru/kibedov/overtime_manager/adpater/salary/SalaryMapperTest.java
@@ -4,16 +4,26 @@ import org.junit.jupiter.api.Test;
 import ru.kibedov.overtime_manager.AbstractMapperTest;
 import ru.kibedov.overtime_manager.adapter.controller.salary.SalaryMapper;
 import ru.kibedov.overtime_manager.adapter.controller.salary.dto.CreateSalaryRequestDto;
+import ru.kibedov.overtime_manager.adapter.controller.salary.dto.SalaryResponseDto;
 import ru.kibedov.overtime_manager.domain.Salary;
 
 public class SalaryMapperTest extends AbstractMapperTest {
 
     @Test
     void salaryMapper_mapToSalary() {
-        CreateSalaryRequestDto dto = readFromFile("/SalaryMapperTest/createSalaryRequestDto_mapToSalary.json", CreateSalaryRequestDto.class);
+        CreateSalaryRequestDto dto = readFromFile("/SalaryMapperTest/mapToSalary/createSalaryRequestDto.json", CreateSalaryRequestDto.class);
 
         Salary salary = SalaryMapper.MAPPER.mapToSalary(dto);
 
-        assertEqualsToFile("/SalaryMapperTest/salary_mapToSalary.json", salary);
+        assertEqualsToFile("/SalaryMapperTest/mapToSalary/salary.json", salary);
+    }
+
+    @Test
+    void salaryMapper_mapToSalaryResponseDto() {
+        Salary salary = readFromFile("/SalaryMapperTest/mapToSalaryResponseDto/salary.json", Salary.class);
+
+        SalaryResponseDto responseDto = SalaryMapper.MAPPER.mapToSalaryResponseDto(salary);
+
+        assertEqualsToFile("/SalaryMapperTest/mapToSalaryResponseDto/salaryResponseDto.json", responseDto);
     }
 }
